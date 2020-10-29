50503326b4c6080e8b877b22cc1fdde3f42511c1
Alejandro.Cortés <gibaleles@gmail.com>
2020-10-28 21:25:33 -0500

    Archivos que no fueron subidos en el ultimo commit @author: me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/* ng serve -o para arrancar el proyecto */
+
+/* ng g class cliente, crea una clase que se llama cliente */
+
+/* ng generate component clientes => Este es el comando para crear una nueva directiva.
+   Crea la carpeta "clientes" con sus respectivos .css, .html, y .ts
+   En este caso la directiva se llama "clientes" :)  */
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent implements OnInit {
+  listaCurso: string[] = ['TypeScript', 'Javascript', 'Java SE', 'C#', 'PHP'];
+  habilitar: boolean = false;
+  constructor() {}
+
+  setHabilitar(): void {
+    this.habilitar = this.habilitar == true ? false : true;
+  }
+
+  ngOnInit(): void {}
+}
