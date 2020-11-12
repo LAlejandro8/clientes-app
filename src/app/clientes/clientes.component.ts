d458eeb4c86515f8d90303518dfb6aa8bf426fae
Alejandro.Cortés <gibaleles@gmail.com>
2020-11-12 15:21:11 -0500

    Fin CRUD front-end @author:Me

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -14,5 +15,47 @@ export class ClientesComponent implements OnInit {
     this.clienteService
       .getClientes()
       .subscribe((clientes) => (this.clientes = clientes));
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: '¿Estas seguro?',
+        text: `¡No se permitira revertir el cambio para ${cliente.nombre} ${cliente.apellido}!`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, eliminalo!',
+        cancelButtonText: 'No, cancela!',
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.clienteService.delete(cliente.id).subscribe((response) => {
+            this.clientes = this.clientes.filter((cli) => cli !== cliente);
+            swalWithBootstrapButtons.fire(
+              'Eliminado!',
+              'El dato ha sido eliminado.',
+              'success'
+            );
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'El dato aun esta seguro :D',
+            'error'
+          );
+        }
+      });
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,23 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
 })
 export class FormComponent implements OnInit {
-  private cliente: Cliente = new Cliente();
-  private titulo: String = 'Crear Cliente';
+  public cliente: Cliente = new Cliente();
+  public titulo: String = 'Crear Cliente';
 
-  constructor(private clienteService: ClienteService, private router: Router) {}
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cargarCliente();
+  }
+
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      let id = params['id'];
+      if (id) {
+        this.clienteService
+          .getCliente(id)
+          .subscribe((cliente) => (this.cliente = cliente));
+      }
+    });
+  }
 
   public create(): void {
-    this.clienteService
-      .create(this.cliente)
-      .subscribe((response) => this.router.navigate(['/clientes']));
+    this.clienteService.create(this.cliente).subscribe((cliente) => {
+      this.router.navigate(['/clientes']);
+      swal.fire(
+        'Nuevo cliente',
+        `¡El cliente ${cliente.nombre} ha sido creado con éxito!`,
+        'success'
+      );
+    });
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente).subscribe((cliente) => {
+      this.router.navigate(['/clientes']);
+      swal.fire(
+        'Cliente actualizado',
+        `¡El cliente ${cliente.nombre} ha sido actualizado con éxito!`,
+        'success'
+      );
+    });
   }
 }
